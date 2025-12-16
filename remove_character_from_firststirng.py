e600c9c06fb945f87816a83b40f3356ecf35fff9
preranajadhav15 <jprerana50@gmail.com>
2025-12-16 15:04:41 +0530

    Delete remove_character_from_firststirng.py

--- remove_character_from_firststirng.py
+++
@@ -1,9 +0,0 @@
-def remove_chars(str1, str2):
-    result = ''
-    for char in str1:
-        if char not in str2:
-            result += char
-    return result
-str1 = "prerana"
-str2 = "jadhav"
-print(remove_chars(str1, str2))
