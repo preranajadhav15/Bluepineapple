d28d2058d316938f7f05db22b8f24f72c86ef945
preranajadhav15 <jprerana50@gmail.com>
2025-12-16 15:01:56 +0530

    Delete volume_of_triangular_prism.py

--- volume_of_triangular_prism.py
+++
@@ -1,6 +0,0 @@
-breath=int(input("Enter the number:"))
-length=int(input("Enter the number:"))
-height=int(input("Enter the number:"))
-volume_of_triangularPrism=(breath*length*height)/2
-print(volume_of_triangularPrism)
-
