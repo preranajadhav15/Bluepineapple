9d0e2a40ef5c853ca060958d95e5a105bcba3f73
preranajadhav15 <jprerana50@gmail.com>
2025-12-16 15:03:56 +0530

    Delete duplicates.py

--- duplicates.py
+++
@@ -1,7 +0,0 @@
-def duplicates(arr):
-    for i in range(len(arr)):
-        for j in range(i+1, len(arr)):
-            if arr[i] == arr[j]:
-                return True   
-    return False
-print(duplicates([1,1,2,3,4]))
