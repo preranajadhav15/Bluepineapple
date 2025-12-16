b9b59dd14e145cb4ab83721061ef10e2eefa81ca
preranajadhav15 <jprerana50@gmail.com>
2025-12-16 15:05:06 +0530

    Delete non_prime.py

--- non_prime.py
+++
@@ -1,9 +0,0 @@
-def non_prime(start,end):
-    nonprime=[]
-    for num in range(start,end+1):
-        for i in range(2,num):
-            if num%i==0:
-                nonprime.append(num)
-                break
-    return nonprime
-print(non_prime(1,10))
