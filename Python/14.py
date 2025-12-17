9e27f6971ebc1ed34bed2df344c22590a2575ad9
preranajadhav15 <jprerana50@gmail.com>
2025-12-17 10:45:03 +0530

    Create 14.py

---
+++ Python/14.py
@@ -0,0 +1,6 @@
+breath=int(input("Enter the number:"))
+length=int(input("Enter the number:"))
+height=int(input("Enter the number:"))
+volume_of_triangularPrism=(breath*length*height)/2
+print(volume_of_triangularPrism)
+
